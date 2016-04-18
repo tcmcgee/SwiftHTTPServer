bd866dc52651cb86042dbe941a064f4580513dc5
Tom McGee <tom@wellss-macbook-pro.local>
2016-04-18 09:16:42 -0500

    Initial Commit

---
+++ SwiftHTTPServer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftHTTPServer
+//
+//  Created by Tom McGee on 4/18/16.
+//  Copyright © 2016 Tom McGee. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
