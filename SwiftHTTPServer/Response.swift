b5a28677f368cee9ff77138066f3473867862c35
Tom McGee <Tom@Toms-MacBook-Pro.local>
2016-04-20 17:31:59 -0500

    WIP: Starting Response

---
+++ SwiftHTTPServer/Response.swift
@@ -0,0 +1,13 @@
+//
+//  Response.swift
+//  SwiftHTTPServer
+//
+//  Created by Tom McGee on 4/20/16.
+//  Copyright © 2016 Tom McGee. All rights reserved.
+//
+
+import Foundation
+
+class Response {
+    
+}
---
+++ SwiftHTTPServerTests/ResponseTests.swift
@@ -0,0 +1,23 @@
+//
+//  ResponseTests.swift
+//  SwiftHTTPServer
+//
+//  Created by Tom McGee on 4/20/16.
+//  Copyright © 2016 Tom McGee. All rights reserved.
+//
+
+import XCTest
+
+class ResponseTests: XCTestCase {
+
+    var response : Response?
+    
+    override func setUp() {
+        super.setUp()
+        response = Response()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    
+
+}
