04b432b4fa9ad93671757d6587160329c459279e
Tom McGee <tmcgee@8thlight.com>
2016-06-07 13:26:32 -0500

    Using bash files in travis

--- SwiftHTTPServer/Configuration.swift
+++ SwiftHTTPServer/Configuration.swift
@@ -1,6 +1,7 @@
 class Configuration {
     
-    static var publicDirectory = "/Users/Tom/8thLight/Apprenticeship/SwiftHTTPServer/cob_spec/public"
+    static var publicDirectory = "./cob_spec/public"
     static var port: UInt16 = 5000
-    static var logDirectory = "/Users/Tom/8thLight/Apprenticeship/SwiftHTTPServer/logs"
+    static var logDirectory = "./logs"
+    static var routes: Dictionary<String,Any> = ["/": Dictionary<HTTPMethods,Route>()]
 }
